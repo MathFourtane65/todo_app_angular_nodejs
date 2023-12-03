9d3853b46ad43ea21588ac673ea2d8827d7e95ed
Mathieu Fourtané <mathieu.fourtane@gmail.com>
2023-12-03 11:03:14 +0100

    correction setup frontend Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path :'todos', component: ListTodosComponent}
+];
